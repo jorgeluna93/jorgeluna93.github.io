2ca30c3bde0d86536f3aac838684969e14ca8831
jorgeluna93 <jluna.dev@icloud.com>
2025-08-07 13:27:46 -0600

    Small Fix 6

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://jorgeluna93.github.io'
+  base:'/jorgeluna93.github.io'
 })
